a19fc14818a85501ea899791e984975728dcaf85
jwoodard839 <123711311+jwoodard839@users.noreply.github.com>
2023-01-27 09:42:19 -0600

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jeff Woodard
+ *         jwoodard@unomaha.edu
+ * Date: 2023/01/27
+ *
+ * A simple name program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jeff Woodard\n");
+
+  return 0;
+}
